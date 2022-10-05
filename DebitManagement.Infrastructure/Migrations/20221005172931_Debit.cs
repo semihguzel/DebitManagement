92cd5b0e78c99f2200d878677efdee94b48c1c47
Semih Güzel <semih_guzel@outlook.com>
2022-10-05 20:34:26 +0300

    Debit migrations

---
+++ DebitManagement.Infrastructure/Migrations/20221005172931_Debit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebitManagement.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Debit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Debits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ActionDescription = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Debits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Debits_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Debits_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debits_ProductId",
+                table: "Debits",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debits_UserId",
+                table: "Debits",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Debits");
+        }
+    }
+}
---
+++ DebitManagement.Infrastructure/Migrations/20221005173112_Debit_Table_Name_Change.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebitManagement.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class DebitTableNameChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debits_Product_ProductId",
+                table: "Debits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debits_Users_UserId",
+                table: "Debits");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Debits",
+                table: "Debits");
+
+            migrationBuilder.RenameTable(
+                name: "Debits",
+                newName: "Debit");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Debits_UserId",
+                table: "Debit",
+                newName: "IX_Debit_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Debits_ProductId",
+                table: "Debit",
+                newName: "IX_Debit_ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Debit",
+                table: "Debit",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debit_Product_ProductId",
+                table: "Debit",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debit_Users_UserId",
+                table: "Debit",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debit_Product_ProductId",
+                table: "Debit");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debit_Users_UserId",
+                table: "Debit");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Debit",
+                table: "Debit");
+
+            migrationBuilder.RenameTable(
+                name: "Debit",
+                newName: "Debits");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Debit_UserId",
+                table: "Debits",
+                newName: "IX_Debits_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Debit_ProductId",
+                table: "Debits",
+                newName: "IX_Debits_ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Debits",
+                table: "Debits",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debits_Product_ProductId",
+                table: "Debits",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debits_Users_UserId",
+                table: "Debits",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
